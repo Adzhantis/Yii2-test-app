bb0ef4995d8772ed6b4a2a87ab8cedcf5b87451a
adzhantis <adzhantis@ukr.net>
2016-03-23 20:34:15 +0200

    final commit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=rgk',
+    'dsn' => 'mysql:host=localhost;dbname=books',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
